c6ae4633a492d8ede91abaea7443f2c3076ec49c
Daniel Ajibewa <ajibewadannyboi@gmail.com>
2025-01-19 06:46:31 +0100

    feat: refactored getImage controller; get image from url using axios

--- controller/image.controller.js
+++ controller/image.controller.js
@@ -2,11 +2,31 @@ import config from "../config.js";
 import axios from "axios"
 import { uploadToS3, getImageFromS3 } from "../utils/awsS3.utils.js";
 import ImageModel from "../models/images.model.js";
-import fs from "fs"
-import path from "path"
 
+// TODO: 
 
-//TODO Need to get an image response using aws_presigner module (try)
+// An helper function that uses image Id to return a image url from AWS
+const getImageNameFromId = async(id) =>{
+    try {
+        // Find the image in the database
+        const image = await ImageModel.findById(id)
+        if(!image){
+            return null
+        }
+        return image.imageName
+    } catch (error) {
+        console.error(`${error}`)
+    }
+}
+
+const fetchImage = async(url) => {
+    try {
+        const response = await axios.get(url, {responseType: "arraybuffer"})
+        return response.data;
+    } catch (error) {
+        console.error(`${error}`)
+    }
+}
 
 export const uploadImage = async(req, res) =>{
     try {
@@ -25,25 +45,32 @@ export const uploadImage = async(req, res) =>{
 export const getImage = async(req, res) =>{
    try{ 
         const { id } = req.params;
-        const __dirname = path.resolve();
-        const localFilePath = path.join(__dirname, "image.jpeg");
-        // Find the image in the database
-        const image = await ImageModel.findById(id)
-        if(!image){
-            return res.status(404).json({"message": "Image not found"})
-        }
-        const imageUrl = await getImageFromS3(image.imageName, config.AWS.bucketName);
+        const imageName = await getImageNameFromId(id);
+        const imageUrl = await getImageFromS3(imageName, config.AWS.bucketName);
         // get Image from the Url
         const imageFileResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' }); // images file response from the url
         const contentType = imageFileResponse.headers["content-type"];
-        const fileName = image.imageName; // get Image file name from DB
+        const fileName = imageName; // get Image file name from DB
         // set the response headers
         res.setHeader("Content-Type", contentType);
         res.setHeader("Content-Disposition", `attachment; filename="${fileName}"`)
-        // send data
+        // send image binary data as a response
         return res.status(200).send(Buffer.from(imageFileResponse.data, "binary"));
     } catch (err){
         return res.status(500).json({"message": `${err}`})
+    }
+}
+
+export const transformImage = async(req, res) => {
+    try {
+        const { id } = req.params;
+        const imageName = await getImageNameFromId(id);
+        const imageUrl = await getImageFromS3(imageName, config.AWS.bucketName);
+        const imageFileBuffer = await fetchImage(imageUrl);
+        // turn buffer into a image
+        return res.status(200).json({"message": "image downloaded Successfully"})
+    } catch (error) {
+        return res.status(500).json({"message": `${error}`})
     }
 }
 
--- routes/images.routes.js
+++ routes/images.routes.js
@@ -1,10 +1,11 @@
 import express from "express"
-import {uploadImage, getImage} from "../controller/image.controller.js"    
+import {uploadImage, getImage, transformImage} from "../controller/image.controller.js"    
 
 const image_router = express.Router();
 
 image_router.post("/", uploadImage);
 image_router.get("/:id", getImage);
+image_router.get("/:id/transform", transformImage);
 
 export default image_router;
 
