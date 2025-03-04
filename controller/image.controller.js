5024638d67fd8411fb3d70230553ca0d61e1d5f7
stateman24 <ajibewadannyboi@gmail.com>
2025-03-03 21:26:41 -0800

    Refactoring to Image Services

--- controller/image.controller.js
+++ controller/image.controller.js
@@ -1,110 +1,125 @@
 import config from "../config.js";
 import axios from "axios";
 import {
-  uploadToS3,
-  getImageFromS3,
-  sendImageToS3,
+    uploadToS3,
+    getImageFromS3,
+    sendImageToS3,
 } from "../utils/awsS3.utils.js";
 import ImageModel from "../models/images.model.js";
 import {
-  imageTransformer,
-  getImageMetadata,
-  getImageNameFromId,
-  fetchImage,
+    imageTransformer,
+    getImageMetadata,
+    getImageNameFromId,
+    fetchImage,
 } from "../utils/image.utils.js";
 import { uploadImageService } from "../services/image.service.js";
 import { StatusCodes } from "http-status-codes";
 
+// upload image controller
 export const uploadImage = async (req, res, next) => {
-  try {
-    if (req.files.images) {
-      const imagesFilesToUpload = req.files.images;
-      const image = await uploadImageService(imagesFilesToUpload, req.user._id);
-      res
-        .status(StatusCodes.CREATED)
-        .json({ data: image, message: "Image(s) uploaded succefully" });
-    } else {
-      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ data: req });
+    try {
+        if (req.files.images) {
+            const imagesFilesToUpload = req.files.images;
+            const image = await uploadImageService(
+                imagesFilesToUpload,
+                req.user._id
+            );
+            res.status(StatusCodes.CREATED).json({
+                data: image,
+                message: "Image(s) uploaded succefully",
+            });
+        } else {
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ data: req });
+        }
+    } catch (error) {
+        next(error);
     }
-  } catch (error) {
-    next(error);
-  }
 };
 
+// get Images controller
+// TODO: Refactor to Image Services
 export const getImages = async (req, res, next) => {
-  try {
-    const images = await ImageModel.find({});
-    if (!images) {
-      return res.status(404).json({ message: "No Image Entries" });
+    try {
+        const images = await ImageModel.find({});
+        if (!images) {
+            return res.status(404).json({ message: "No Image Entries" });
+        }
+        return res.status(200).json(images);
+    } catch (error) {
+        return res.status(500).json(error);
     }
-    return res.status(200).json(images);
-  } catch (error) {
-    return res.status(500).json(error);
-  }
 };
 
+// get a single image by Id
+// TODO: Refactore to Image Services
 export const getImage = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const image = await ImageModel.findById(id);
-    if (!image) {
-      return res.status(404).json({ message: "Image not found" });
+    try {
+        const { id } = req.params;
+        const image = await ImageModel.findById(id);
+        if (!image) {
+            return res.status(404).json({ message: "Image not found" });
+        }
+        return res.status(200).json(image);
+    } catch (error) {
+        return res.status(500).json({ message: `${error}` });
     }
-    return res.status(200).json(image);
-  } catch (error) {
-    return res.status(500).json({ message: `${error}` });
-  }
 };
 
+// downloadImage an Image form the cloud
+// TODO: Refactor to Image Service
 export const downloadImage = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const imageName = await getImageNameFromId(id);
-    const imageUrl = await getImageFromS3(
-      imageName,
-      config.AWS.bucketName,
-      3600,
-    );
-    // get Image from the Url
-    const imageFileResponse = await axios.get(imageUrl, {
-      responseType: "arraybuffer",
-    }); // images file response from the url
-    const contentType = imageFileResponse.headers["content-type"];
-    const fileName = imageName; // get Image file name from DB
-    // set the response headers
-    res.setHeader("Content-Type", contentType);
-    res.setHeader("Content-Disposition", `attachment; filename="${fileName}"`);
-    // send image binary data as a response
-    return res.status(200).send(Buffer.from(imageFileResponse.data, "binary"));
-  } catch (err) {
-    return res.status(500).json({ message: `${err}` });
-  }
+    try {
+        const { id } = req.params;
+        const imageName = await getImageNameFromId(id);
+        const imageUrl = await getImageFromS3(
+            imageName,
+            config.AWS.bucketName,
+            3600
+        );
+        // get Image from the Url
+        const imageFileResponse = await axios.get(imageUrl, {
+            responseType: "arraybuffer",
+        }); // images file response from the url
+        const contentType = imageFileResponse.headers["content-type"];
+        const fileName = imageName; // get Image file name from DB
+        // set the response headers
+        res.setHeader("Content-Type", contentType);
+        res.setHeader(
+            "Content-Disposition",
+            `attachment; filename="${fileName}"`
+        );
+        // send image binary data as a response
+        return res
+            .status(200)
+            .send(Buffer.from(imageFileResponse.data, "binary"));
+    } catch (err) {
+        return res.status(500).json({ message: `${err}` });
+    }
 };
 
+// Transform Image based on transformation parameters
+// TODO: Refactor to Image Service
 export const transformImage = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const transformationParams = req.body.transformations;
-    const imageName = await getImageNameFromId(id);
-    const imageUrl = await getImageFromS3(imageName, config.AWS.bucketName);
-    const imageFileBuffer = await fetchImage(imageUrl);
-    const transformedImageBuffer = await imageTransformer(
-      imageFileBuffer,
-      transformationParams,
-    );
-    // upload image back to the cloud
-    const uploadResult = await sendImageToS3(
-      transformedImageBuffer,
-      config.AWS.bucketName,
-      imageName,
-    );
-    return res.status(200).json({ message: "Image Transformed" });
-  } catch (error) {
-    return res.status(500).json({ message: `${error}` });
-  }
+    try {
+        const { id } = req.params;
+        const transformationParams = req.body.transformations;
+        const imageName = await getImageNameFromId(id);
+        const imageUrl = await getImageFromS3(imageName, config.AWS.bucketName);
+        const imageFileBuffer = await fetchImage(imageUrl);
+        const transformedImageBuffer = await imageTransformer(
+            imageFileBuffer,
+            transformationParams
+        );
+        // upload image back to the cloud
+        const uploadResult = await sendImageToS3(
+            transformedImageBuffer,
+            config.AWS.bucketName,
+            imageName
+        );
+        return res.status(200).json({ message: "Image Transformed" });
+    } catch (error) {
+        return res.status(500).json({ message: `${error}` });
+    }
 };
-
-// TODO: make all controllers to only hand requests and send response
-// TODO: Split the some controller functionalities to the services module e.g transform
 
 export default uploadImage;
--- services/auth.service.js
+++ services/auth.service.js
@@ -6,45 +6,45 @@ import config from "../config.js";
 import jwt from "jsonwebtoken";
 
 export const signUpService = async (signUpData) => {
-  if (!signUpData) {
-    throw createHttpError(StatusCodes.BAD_REQUEST, "User input required");
-  }
-  const { password, passwordRepeat, email } = signUpData;
+    if (!signUpData) {
+        throw createHttpError(StatusCodes.BAD_REQUEST, "User input required");
+    }
+    const { password, passwordRepeat, email } = signUpData;
 
-  const existingUser = await User.findOne({ email });
-  if (existingUser) {
-    throw createHttpError(StatusCodes.BAD_REQUEST, "Email already exists");
-  }
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+        throw createHttpError(StatusCodes.BAD_REQUEST, "Email already exists");
+    }
 
-  if (password !== passwordRepeat) {
-    throw createHttpError(
-      StatusCodes.BAD_REQUEST,
-      "Repeat Password does not match",
-    );
-  }
+    if (password !== passwordRepeat) {
+        throw createHttpError(
+            StatusCodes.BAD_REQUEST,
+            "Repeat Password does not match"
+        );
+    }
 
-  const user = await User.create(signUpData);
+    const user = await User.create(signUpData);
 
-  return user;
+    return user;
 };
 
 export const loginService = async (loginData) => {
-  if (!loginData) {
-    throw createHttpError(
-      StatusCodes.INTERNAL_SERVER_ERROR,
-      "User Login details not provided",
-    );
-  }
+    if (!loginData) {
+        throw createHttpError(
+            StatusCodes.INTERNAL_SERVER_ERROR,
+            "User Login details not provided"
+        );
+    }
 
-  const user = await User.findOne({ email: loginData.email });
-  if (!user) {
-    throw createHttpError(StatusCodes.NOT_FOUND, "User does not exists");
-  }
-  const isMatch = await bcrypt.compare(loginData.password, user.password);
-  if (!isMatch) {
-    throw createHttpError(StatusCodes.BAD_REQUEST, "Incorrect Password");
-  }
-  const expiresIn = 3600;
-  const token = jwt.sign({ _id: user._id }, config.JWT_SECRET, { expiresIn });
-  return { user, token };
+    const user = await User.findOne({ email: loginData.email });
+    if (!user) {
+        throw createHttpError(StatusCodes.NOT_FOUND, "User does not exists");
+    }
+    const isMatch = await bcrypt.compare(loginData.password, user.password);
+    if (!isMatch) {
+        throw createHttpError(StatusCodes.BAD_REQUEST, "Incorrect Password");
+    }
+    const expiresIn = 3600;
+    const token = jwt.sign({ _id: user._id }, config.JWT_SECRET, { expiresIn });
+    return { user, token };
 };
